e8fda6275dbbe0a6fc0a04dd2db83e0c906911fd
SigBombrero <edosman15@gmail.com>
2023-04-27 18:59:02 -0700

    Initial commit

---
+++ alias.sh
@@ -0,0 +1,29 @@
+alias ll="ls -lia"
+alias cls="clear"
+alias gst="git status"
+alias k="kubectl"
+alias kdp="kubectl describe pod"
+alias kds="kubectl describe statefulset"
+alias py3="python3"
+
+# output all pods with their associated container images
+# takes 1 optional argument which serves as a grep filter
+function kpi {
+    if [[ -n "$1" ]]
+    then
+        kubectl get pods -o jsonpath="{range.items[*]}(.metadata.name){' '}{.spec.containers[*].image}{'\n'}{end}" | grep $1 | column -t
+    else
+        kubectl get pods -o jsonpath="{range.items[*]}(.metadata.name){' '}{.spec.containers[*].image}{'\n'}{end}" | column -t
+    fi
+}
+
+# output all events for a kubernetes object
+# expects object name as argument
+function kge {
+    if [[ -n "$1" ]]
+    then
+       kubectl get event --field-selector involvedObject.name="$1"
+    else
+        echo "Please enter the name of the object for which you want the events (e.g. pod, statefulset, etc.)."
+    fi   
+}
